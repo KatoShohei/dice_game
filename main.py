a2fe3c2b9f8a431a05eb192ad09ea07ec180455b
KatoShohei <kato.shohei.86n@kyoto-u.jp>
2025-06-28 11:57:06 +0900

    サイコロゲームのバトルロジックを実装し、最終結果を表示する機能を追加。uv.lockファイルを新規作成。

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+from enum import Enum, auto
 import random
+
+
+class Result(Enum):
+    WIN = auto()
+    LOSE = auto()
+    DRAW = auto()
 
 
 def roll_dice(sides: int = 6):
     return random.randint(1, sides)
 
 
-def roll_ndice(n=1, sides: int = 6):
+def roll_ndice(n: int = 1, sides: int = 6):
     return (random.randint(1, sides) for _ in range(n))
 
 
@@ -16,15 +23,38 @@ def battle():
     print(f"CPUの出目: {cpu_dice}")
     if your_dice > cpu_dice:
         print("あなたの勝ち!")
+        return Result.WIN
     elif your_dice == cpu_dice:
         print("惜しい! 引き分け")
+        return Result.DRAW
     else:
         print("残念。あなたの負け...")
+        return Result.LOSE
 
 
 def main():
     print("Hello from dice-game!")
-    battle()
+    print("CPUとダイスを振り合い、出目が大きい方が勝ちです。")
+    print("3回勝負し、最終的な勝ち数が多い方が勝者とします。")
+    d = {"win": 0, "draw": 0, "lose": 0}
+    for i in range(3):
+        print(f"### 第{i}回戦 ###")
+        res = battle()
+        if res is Result.WIN:
+            d["win"] += 1
+        elif res is Result.DRAW:
+            d["draw"] += 1
+        else:
+            d["lose"] += 1
+    print()
+    print("## 最終結果発表! ##")
+    print(f"{d['win']}勝{d['lose']}敗{d['draw']}分け")
+    if d["win"] > d["lose"]:
+        print("おめでとう! あなたの勝利!!")
+    elif d["win"] == d["lose"]:
+        print("惜しい! 引き分け")
+    else:
+        print("残念。あなたの負け...")
     input("Press Enter to exit")
 
 
